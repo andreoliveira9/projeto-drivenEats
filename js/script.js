c55ea3e5ba6de1ddd945d4ddc22683adf1f0b7e8
Andre Oliveira <andreprogramador10@gmail.com>
2022-01-26 14:05:19 -0300

    adição do icone de confirmacao quando um determinado produto é selecionado

--- js/script.js
+++ js/script.js
@@ -1,14 +1,21 @@
 function selecionaItem(produto){
-    produtos = produto.parentNode
+    let produtos = produto.parentNode
+
     produtos.childNodes.forEach(function(p){
         if(p.classList){
              if(p.classList.contains('selecionado')){
                     p.classList.remove('selecionado')
+                    if(p.lastElementChild.lastElementChild){
+                        p.lastElementChild.lastElementChild.style.display = 'none'
+                    }
+                    
+                    
                 }
             }
         })
     produto.classList.add("selecionado")
-    contaSelecionados()
+    produto.lastElementChild.lastElementChild.style.display = 'block'
+    
     if(contaSelecionados() == 3){
         let botao = document.querySelector('button')
             botao.disabled = false;
@@ -18,5 +25,6 @@ function selecionaItem(produto){
 }
 function contaSelecionados(){
     let selecionados = document.querySelectorAll('.selecionado')
+
     return selecionados.length
 }
